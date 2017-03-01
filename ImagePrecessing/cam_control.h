0d6ee3cd80c324d362458f81390d69550e357300
chankyoo <chankyoo.moon@gmail.com>
2017-03-02 06:07:02 +0900

    cammand input

---
+++ ImagePrecessing/cam_control.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "opencv_base.h"
+
+class cam_control
+{
+public:
+	cam_control();
+	~cam_control();
+
+	int do_cam (void);
+};
+
--- ImagePrecessing/main.cpp
+++ ImagePrecessing/main.cpp
@@ -1,40 +1,25 @@
-#include "opencv2/opencv.hpp"  
 #include <iostream>  
-
-using namespace cv;
 using namespace std;
 
-
+#include "cam_control.h"
 
 int main(int, char**)
 {
-	//웹캡으로 부터 데이터 읽어오기 위해 준비  
-	VideoCapture capture(0);
-	if (!capture.isOpened())
+	char cIputCase[5];
+	cout << "----------------" << endl;
+	cout << "[0] Exit" << endl;
+	cout << "[1] Web Cam Test" << endl;
+	cin >> cIputCase;
+	cout << "----------------" << endl;
+	
+	if (strcmp(cIputCase, "0"))
 	{
-		cout << "웹캠을 열수 없습니다." << endl;
-		return 1;
+		return 0;
 	}
-
-	//캡처 영상을 320x240으로 지정  
-	capture.set(CAP_PROP_FRAME_WIDTH, 320);
-	capture.set(CAP_PROP_FRAME_HEIGHT, 240);
-
-
-	Mat frame;
-	namedWindow("input", 1);
-
-
-	while (1)
+	else if (strcmp(cIputCase, "1"))
 	{
-		//웹캡으로부터 한 프레임을 읽어옴  
-		capture >> frame;
-
-		//화면에 영상을 보여줌
-		imshow("input", frame);
-
-		//ESC키 누르면 종료
-		if (waitKey(1) == 27) break;
+		cam_control cCam;
+		cCam.do_cam();
 	}
 
 	return 0;
